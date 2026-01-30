f3fc0decdbc90e84efea09fc571bd75bac1fd2a3
Jedyokey <jedyokey29@gmail.com>
2026-01-30 09:54:02 +0000

    Add landing page layout and shared UI components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,16 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-import { Inter } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header"
+import Footer from "@/components/common/footer"
+import { ClerkProvider } from "@clerk/nextjs"
 
 // fonts 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-const inter = Inter({
-  subsets: ["latin"]
-  // variable: "--font-inter",
-})
+const outfit = Outfit({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "iBuiltThis",
-  description: "iBuiltThis is a platform for building and sharing your own projects.",
+  title: "iBuiltThis - Share Your Creations, Discover New Launches",
+  description: "A community platform for creators to showcase their apps, AI tools, SaaS products, creative projects Authentic launches, real builders, genuine feedback.",
 };
 
 export default function RootLayout({
@@ -30,14 +19,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        <header>iBuiltThis</header>
-        {children}
-        <footer>iBuiltThis Inc. All rights reserved.</footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${outfit.className} antialiased`}
+        >
+          <Header />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
