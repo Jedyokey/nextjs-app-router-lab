c98dd4788f1320a2560e3bd3f4ae8797e6fb9a73
Jedyokey <jedyokey29@gmail.com>
2026-02-16 09:10:46 +0000

    Add Next.js caching for products and components

--- lib/products/product-select.ts
+++ lib/products/product-select.ts
@@ -3,6 +3,8 @@ import { products } from "@/db/schema";
 import { desc, eq } from "drizzle-orm";
 
 export async function getFeaturedProducts() {
+    "use cache";
+
     const productsData = await db
         .select()
         .from(products)
@@ -14,13 +16,22 @@ export async function getFeaturedProducts() {
     return productsData;
 }
 
+export async function getAllProducts() {
+    const productsData = await db
+        .select()
+        .from(products)
+        .orderBy(desc(products.voteCount))
+        .where(eq(products.status, "approved")); // Only fetch approved products
+    return productsData;
+}
+
 export async function getRecentlyLaunchedProducts() {
-    const productsData = await getFeaturedProducts();
+    const productsData = await getAllProducts();
     const oneWeekAgo = new Date();
     oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
 
     return productsData.filter(
-        (products) => products.createdAt && 
-        new Date(products.createdAt.toISOString()) >= oneWeekAgo
+        (product) => product.createdAt && 
+        new Date(product.createdAt) >= oneWeekAgo
     );
 }
